a0a2742a4fd4439a50597975e685d0cfa0388d1d
Anton Taraskin <akdh060909@gmail.com>
2026-05-27 03:27:37 +0300

    Сделал классы, и немного затронул базу данных

---
+++ Project_Turizm_Zal/Models/ExhibitViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Project_Turizm_Zal.Models
+{
+    public class ExhibitViewModel
+    {
+            public Guid Id { get; set; }
+
+            public string Name { get; set; } = string.Empty;
+
+            public List<string> Images { get; set; } = new();
+
+            public string Description { get; set; } = string.Empty;
+
+            public string ExhibitionName { get; set; } = string.Empty;
+    }
+}
