79da2c52e8b4084a100d5cce9697c2eb1d7c2c0e
TsagArinaa <tsagaraevaarina@gmail.com>
2026-05-31 23:37:27 +0300

    создала страницу зала Древнего Египта, контроллер +view

---
+++ Project_Turizm_Zal/Controllers/HallsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Project_Turizm_Zal.Controllers
+{
+    public class HallsController : Controller
+    {
+        public IActionResult HallEgipt()
+        {
+            return View();
+        }
+    }
+}
