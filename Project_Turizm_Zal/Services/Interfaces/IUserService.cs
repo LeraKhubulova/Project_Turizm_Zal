fec849cd7aed49143d021fadd520cbcff1180e6a
Anton Taraskin <akdh060909@gmail.com>
2026-06-01 22:54:28 +0300

    Добавил роль админа и поправил все, что с этим связано

---
+++ Project_Turizm_Zal/Services/Interfaces/IUserService.cs
@@ -0,0 +1,11 @@
+﻿using Project_Turizm_Zal.Models;
+
+namespace Project_Turizm_Zal.Services
+{
+    public interface IUserService
+    {
+        Task<bool> IsUserExists(string email, CancellationToken cancellationToken);
+        Task<User> Login(string email, string password, CancellationToken cancellationToken);
+        Task<bool> Register(User user, CancellationToken cancellationToken);
+    }
+}
